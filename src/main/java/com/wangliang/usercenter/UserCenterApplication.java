33be26f53980c4da452580a4fc34f43abf8bf733
wangliang <3130187893@qq.com>
2024-04-09 21:21:32 +0800

    用户中心后端项目

---
+++ src/main/java/com/wangliang/usercenter/UserCenterApplication.java
@@ -0,0 +1,15 @@
+package com.wangliang.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.wangliang.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wangliang/usercenter/controller/UserController.java
@@ -0,0 +1,95 @@
+package com.wangliang.usercenter.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.wangliang.usercenter.model.domain.User;
+import com.wangliang.usercenter.model.domain.request.UserLoginRequest;
+import com.wangliang.usercenter.model.domain.request.UserRegisterRequest;
+import com.wangliang.usercenter.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.wangliang.usercenter.constant.UserConstant.ADMIN_ROLE;
+import static com.wangliang.usercenter.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 用户接口
+ *
+ * @author wangliang
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        if (userRegisterRequest == null) {
+            return null;
+        }
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return null;
+        }
+        return userService.userRegister(userAccount, userPassword, checkPassword);
+    }
+
+    @PostMapping("/login")
+    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        if (userLoginRequest == null) {
+            return null;
+        }
+        String userAccount = userLoginRequest.getUserAccount();
+        String userPassword = userLoginRequest.getUserPassword();
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            return null;
+        }
+        return userService.userLogin(userAccount, userPassword, request);
+    }
+
+    @GetMapping("/search")
+    public List<User> searchUsers(String username, HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            return new ArrayList<>();
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            queryWrapper.like("username", username);
+        }
+        List<User> userList = userService.list(queryWrapper);
+        return userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+    }
+
+    @PostMapping("/delete")
+    public boolean deleteUsers(@RequestBody long id, HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            return false;
+        }
+        if (id <= 0) {
+            return false;
+        }
+        return userService.removeById(id);
+    }
+
+    /**
+     * 是否为管理员
+     *
+     * @param request
+     * @return
+     */
+    private boolean isAdmin(HttpServletRequest request) {
+        // 1.仅管理员可查询
+        Object userobj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User user = (User) userobj;
+        return user != null && user.getUserRole() == ADMIN_ROLE;
+    }
+}
